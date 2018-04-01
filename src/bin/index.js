d2acd0e4a7737232aa372ef3378f8a68906db93f
Benjamin H Boruff <ben@boruff.me>
2018-03-31 20:56:25 -0400

    books schema bug fix

--- src/bin/index.js
+++ src/bin/index.js
@@ -8,5 +8,5 @@ const port = 3000;
 const host = '127.0.0.1';
 
 server.listen(port, host, ()=>{
-    console.log(`mini-graphql-server http://${host}:${port}:/graphiql ...`);
+    console.log(`mini-graphql-server http://${host}:${port}/graphiql ...`);
 });
